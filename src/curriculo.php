ccd2b08c68c1a12aa9bec7d84d3ab9f61e5607a9
ArthurAssuncao <arthur.xx92@gmail.com>
2013-04-03 20:54:36 -0300

    modificado: estrutura da pagina Curriculo para funcionar com o novo template

--- src/curriculo.php
+++ src/curriculo.php
@@ -1,12 +1,14 @@
-<?php 
-	$title = 'Arthur Assunção - Curriculo';
-	$description = 'Curriculo de Arthur Assunção';
-	$keywords = 'Curriculo, Arthur Assunção, Instituto Federal do Sudeste de Minas Gerais, Barbacena, Sistemas para Internet, Programação, github';
-	
-	require_once('template/header.php');
-	
+<?php
+	require('template/Pagina.class.php');
+	$pagina = new Pagina();
+	$pagina->setTitle('Curriculo - Arthur Assunção');
+	$pagina->setDescription('Curriculo de Arthur Assunção');
+	$pagina->setKeywords('Curriculo, Arthur Assunção, Instituto Federal do Sudeste de Minas Gerais, Barbacena, Sistemas para Internet, Programação, github');
+	$pagina->setCanonical($pagina->createCanonicalLink());
+	$pagina->iniciaConteudo();
 ?>
-    <h3>Currículo</h3>
-<?php 
-	require_once('template/footer.php');
+	<h3>Currículo</h3>
+<?php
+	$pagina->finalizaConteudo();
+	echo $pagina->renderizar();
 ?>
