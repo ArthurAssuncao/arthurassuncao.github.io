26ffff55dca72ee7f8fd972ab2a76bab92eae0dc
ArthurAssuncao <arthur.xx92@gmail.com>
2013-04-04 10:37:55 -0300

    adicionado: metodo renderizar sobreescrito para fazer o cache da pagina

--- src/template/PaginaCached.class.php
+++ src/template/PaginaCached.class.php
@@ -1,7 +1,18 @@
 <?php
-include('Pagina.class.php');
+require('Pagina.class.php');
+require('../min/lib/Minify/Cache/File.php');
+
 // classe Pagina com a funcao de Cache
 class PaginaCached extends Pagina {
+	$arquivoCache;
+	$arquivoId;
 
+	public function renderizar($template='template.php') {
+		$htmlRenderizado = Parent::renderizar();
+		$this->arquivoCache = Minify_Cache_File();
+		$this->arquivoCache.store($arquivoId, $htmlRenderizado);
+	}
+
+	public generateId(){}
 }
 ?>
