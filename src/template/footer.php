4f43486eea11bd98e4572ae275be0f0533171743
ArthurAssuncao <arthur.xx92@gmail.com>
2013-04-03 22:21:41 -0300

    corrigido: template

--- src/template/footer.php
+++ src/template/footer.php
@@ -42,13 +42,3 @@
 			</div>
 		</div>
 	</footer>
-	<!-- Scripts rodam mais rapidos e de forma melhor estando no fim da pagina -->
-	<?php //<script type="text/javascript" src="/js/jquery/jquery-1.8.3.min.js"></script> ?>
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-	<script type="text/javascript" src="/min/?f=/js/bootstrap/bootstrap.min.js,/js/site.js"></script>
-<?php 
-	echo $this->createTagsJS($this->links_js_footer);
-	echo "<script type='text/javascript'>$this->embedded_js_footer</script>";
- ?>
-	</body>
-</html>
