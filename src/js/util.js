e2f82944949754a4ef3d069ba432455d24a979dc
ArthurAssuncao <arthur.xx92@gmail.com>
2013-04-03 21:49:19 -0300

    adicionado: arquivo javascript com funcoes utilitarias

---
+++ src/js/util.js
@@ -0,0 +1,17 @@
+function getURLParameters() {
+	var searchString = window.location.search.substring(1);
+	var params = searchString.split("&");
+	var hash = {};
+
+	for (var i = 0; i < params.length; i++) {
+		var val = params[i].split("=");
+		hash[unescape(val[0])] = unescape(val[1]);
+	}
+	return hash;
+}
+
+function getURLParameter(name) {
+    return decodeURIComponent(
+        (location.search.match(RegExp("[?|&]"+name+'=(.+?)(&|$)'))||[,null])[1]
+    );  
+}
