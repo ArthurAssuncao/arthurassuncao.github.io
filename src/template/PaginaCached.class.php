815a8abf96fda825e50f73597c3565b5f593e5d2
ArthurAssuncao <arthur.xx92@gmail.com>
2013-04-04 23:57:19 -0300

    corrigido: classe Pagina com a funcao de Cache

--- src/template/PaginaCached.class.php
+++ src/template/PaginaCached.class.php
@@ -1,18 +1,76 @@
 <?php
 require('Pagina.class.php');
-require('../min/lib/Minify/Cache/File.php');
 
 // classe Pagina com a funcao de Cache
 class PaginaCached extends Pagina {
-	$arquivoCache;
-	$arquivoId;
+	private $arquivoCache;
+	private $arquivoCacheId;
+	private $caminhoArquivo;
 
-	public function renderizar($template='template.php') {
-		$htmlRenderizado = Parent::renderizar();
-		$this->arquivoCache = Minify_Cache_File();
-		$this->arquivoCache.store($arquivoId, $htmlRenderizado);
+	public function PaginaCached($caminhoArquivo) { //__FILE__
+		parent::__construct();
+		require($_SERVER['DOCUMENT_ROOT'].'/min/lib/Minify/Cache/File.php');
+
+		$this->caminhoArquivo = $caminhoArquivo;
 	}
 
-	public generateId(){}
+	public function renderizar($template='template.php') {
+		if($this->exibir_so_conteudo == false){
+			$htmlRenderizado = $this->criaCache($template);
+		}
+		else{
+			$htmlRenderizado = parent::renderizar($template);
+		}
+		return $htmlRenderizado;
+	}
+
+	public function criaCache($template='template.php'){
+		$this->arquivoCache = new Minify_Cache_File($_SERVER['DOCUMENT_ROOT'].'/min/cache/');
+		$md5Arquivo = $this->geraCacheMd5();
+		$md5Template = $this->geraTemplateMd5();
+		$nomeCache = $this->geraNomeCache();
+		$cachesAtual = glob($_SERVER['DOCUMENT_ROOT']."/min/cache/{$nomeCache}*");
+		$this->arquivoCacheId = $this->geraCacheId($md5Arquivo, $md5Template);
+		$htmlRenderizado = null;
+		if(count($cachesAtual) == 1){
+			if(basename($cachesAtual[0]) == $this->arquivoCacheId){
+				//cache ja criado
+				$htmlRenderizado = $this->arquivoCache->fetch($this->arquivoCacheId);
+			}
+			else{
+				@unlink($cachesAtual[0]); //apaga o cache
+			}
+		}
+		if($htmlRenderizado == null){
+			$htmlRenderizado = parent::renderizar($template);
+			$this->arquivoCache->store($this->arquivoCacheId, $htmlRenderizado); 
+		}
+		return $htmlRenderizado;
+	}
+
+	public function geraCacheMd5(){
+		$md5Arquivo = md5_file($this->caminhoArquivo);
+        return $md5Arquivo;
+	}
+
+	public function geraTemplateMd5(){
+        $md5Template = md5_file($_SERVER['DOCUMENT_ROOT'].'/template/template.php');
+        return $md5Template;
+	}
+
+	public function geraNomeCache($prefixo='pagina'){
+		//$nome = preg_replace('/[^a-zA-Z0-9\\.=_,]/', '', self::$_controller->selectionId);
+        //$nome = preg_replace('/\\.+/', '.', $nome);
+        $nome = basename($this->caminhoArquivo, ".php");
+        $nome = substr($nome, 0, 200 - 34 - strlen($prefixo));
+        return "{$prefixo}_{$nome}";
+	}
+
+	public function geraCacheId($md5Arquivo, $md5Template, $nome=''){
+		if(!$nome){
+	        $nome = $this->geraNomeCache();
+	    }
+        return "{$nome}_{$md5Arquivo}_{$md5Template}";
+    }
 }
 ?>
