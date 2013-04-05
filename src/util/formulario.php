edb0fba466e0bec533295b5b2d0757a382fd737a
ArthurAssuncao <arthur.xx92@gmail.com>
2013-04-05 16:44:59 -0300

    adicionado: alt aos input seguindo a especificacao da w3c para acessibilidade a wcag

--- src/util/formulario.php
+++ src/util/formulario.php
@@ -10,7 +10,7 @@ function cria_input($texto_campo, $nome_campo, $tipo='text', $required=false, $p
 	echo "<label class='control-label $required' for='$nome_campo'>$texto_campo</label>\n";
 	echo '<div class="controls">'."\n";
 	$pattern = $pattern ? "pattern=\"$pattern\"" : '';
-	echo "<input name='$nome_campo' type='$tipo' class='input-xxlarge' id='$nome_campo' $required placeholder='$placeholder' title='$title' $pattern>\n";
+	echo "<input name='$nome_campo' type='$tipo' class='input-xxlarge' id='$nome_campo' $required placeholder='$placeholder' title='$title' alt='$texto_campo' $pattern>\n";
 	if($help_text) echo "<p class='help-block'>$help_text</p>\n";
 	echo '</div>';
 	//echo '</div>';
@@ -21,7 +21,7 @@ function cria_textarea($texto_campo, $nome_campo, $linhas='1', $colunas='10', $r
 	//echo '<div class="control-group">'."\n";
 	echo "<label class='control-label $required' for='$nome_campo'>$texto_campo</label>\n";
 	echo '<div class="controls">'."\n";
-	echo "<textarea name='$nome_campo' cols='$colunas' rows='$linhas' class='input-xxlarge' id='$nome_campo' $required placeholder='$placeholder' title='$title'></textarea>\n";
+	echo "<textarea name='$nome_campo' cols='$colunas' rows='$linhas' class='input-xxlarge' id='$nome_campo' $required placeholder='$placeholder' title='$title' alt='$texto_campo'></textarea>\n";
 	if($help_text) echo "<p class='help-block'>$help_text</p>\n";
 	echo '</div>';
 	//echo '</div>';
