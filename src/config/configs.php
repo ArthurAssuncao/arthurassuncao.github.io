92d8ac553d10050d249c9530dbb5c0f4959468b3
ArthurAssuncao <arthur.xx92@gmail.com>
2013-04-07 20:59:49 -0300

    adicionado: diretorio para os caches e com as configs

---
+++ src/config/configs.php
@@ -0,0 +1,8 @@
+<?php
+	//email
+	$nome_remetente = "ArthurAssuncao.com";
+	$email_remetente = "seuemail@dominio.com";
+	$senha_remetente = "senha"; // senha
+	$email_destinatario = "outroemail@dominio.com";
+	$nome_destinatario = "Outro email";
+?>
