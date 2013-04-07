1c497cb5cd5646d6f82b31f832b0c1ba3315a0f0
ArthurAssuncao <arthur.xx92@gmail.com>
2013-04-07 20:30:35 -0300

    modificado: local do username e senha do email

---
+++ src/config/configs.php
@@ -0,0 +1,8 @@
+<?php
+	//email
+	$nome_remetente = "ArthurAssuncao.com";
+	$email_remetente = "youremail@yourdomain.com";
+	$senha_remetente = "password"; // senha
+	$email_destinatario = "anotheremail@yourdomain.com";
+	$nome_destinatario = "Name destination";
+?>
