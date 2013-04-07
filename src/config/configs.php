d9b17993fee39c22aa3e124e658507cfb27ec17b
ArthurAssuncao <arthur.xx92@gmail.com>
2013-04-07 20:36:32 -0300

    removido: arquivo configs do cache do git

--- src/config/configs.php
+++
@@ -1,8 +0,0 @@
-<?php
-	//email
-	$nome_remetente = "ArthurAssuncao.com";
-	$email_remetente = "youremail@yourdomain.com";
-	$senha_remetente = "password"; // senha
-	$email_destinatario = "anotheremail@yourdomain.com";
-	$nome_destinatario = "Name destination";
-?>
