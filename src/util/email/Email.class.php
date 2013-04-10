212e6273e87ecb68d594d57cc2ecc4e8a9628d1b
ArthurAssuncao <arthur.xx92@gmail.com>
2013-04-10 15:08:51 -0300

    adicionado: validacao do email

--- src/util/email/Email.class.php
+++ src/util/email/Email.class.php
@@ -2,14 +2,18 @@
 	include("phpmailer/class.phpmailer.php");
 class Email{
 	public $phpMailer;
+	private $nome;
+	private $email;
+	private $mensagem;
+	private $assunto;
 
 	public function __construct($nome, $email, $mensagem, $assunto){
 		require($_SERVER['DOCUMENT_ROOT'].'/config/configs.php');
 
-		$nome = strip_tags($nome);
-		$email = strip_tags($email);
-		$mensagem = strip_tags($mensagem);
-		$assunto = strip_tags($assunto);
+		$this->nome = strip_tags($nome);
+		$this->email = strip_tags($email);
+		$this->mensagem = strip_tags($mensagem);
+		$this->assunto = strip_tags($assunto);
 
 		//instancia a objetos
 		$this->phpMailer = new PHPMailer();
@@ -41,26 +45,31 @@ class Email{
 
 	    // informando a quem devemos responder 
 		//ou seja para o mail inserido no formulario
-		$this->phpMailer->AddReplyTo("$email", "$nome");
+		$this->phpMailer->AddReplyTo("$this->email", "$this->nome");
 
 		//criando o codigo html para enviar no email
 		$msg = "<h3>Mensagem Enviada do site ArthurAssuncao.com pelo Formulario de Contato</h3><br><br>\n";
-		$msg .= "<strong> Nome:</strong> $nome<br>\n";
-		$msg .= "<strong> E-mail:</strong> $email<br>\n";
-		$msg .= "<strong> Mensagem:</strong> $mensagem<br>\n";
+		$msg .= "<strong> Nome:</strong> $this->nome<br>\n";
+		$msg .= "<strong> E-mail:</strong> $this->email<br>\n";
+		$msg .= "<strong> Mensagem:</strong> $this->mensagem<br>\n";
 
 		//adicionando o assunto do email
-		$this->phpMailer->Subject = 'Contato via ArthurAssuncao.com: '$assunto;
+		$this->phpMailer->Subject = 'Contato via ArthurAssuncao.com: '$this->assunto;
 		//adicionando o html no corpo do email
 		$this->phpMailer->Body = $msg;
 	}
 
 	public function envia(){
 		//enviando e retornando o status de envio
-		if(!$this->phpMailer->Send()){
-			return 'Houve um erro ao  enviar o email! '.$this->phpMailer->ErrorInfo;
-			//$this->phpMailer->ErrorInfo informa onde ocorreu o erro 
-			//exit;
+		if (filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+			if(!$this->phpMailer->Send()){
+				return 'Houve um erro ao  enviar o email! '.$this->phpMailer->ErrorInfo;
+				//$this->phpMailer->ErrorInfo informa onde ocorreu o erro 
+				//exit;
+			}
+		}
+		else{
+			return 'O Email informado é inválido';
 		}
 		return '';
 	}
