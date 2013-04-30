bd535fc784e390f496022286ff7862ba4da897a1
ArthurAssuncao <arthur.xx92@gmail.com>
2013-04-30 10:17:31 -0300

    corrigido: layout responsivo no ie8

--- src/template/template.php
+++ src/template/template.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
-<html lang="<?php echo $this->lang ?>" prefix="og: http://ogp.me/ns#">
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="<?php echo $this->lang ?>" prefix="og: http://ogp.me/ns#"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang="<?php echo $this->lang ?>" prefix="og: http://ogp.me/ns#"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="<?php echo $this->lang ?>" prefix="og: http://ogp.me/ns#"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="<?php echo $this->lang ?>" prefix="og: http://ogp.me/ns#"> <!--<![endif]-->
 <?php
     // Header
     require('header.php');
@@ -34,6 +37,7 @@
     <!-- Scripts rodam mais rapidos e de forma melhor estando no fim da pagina -->
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script type="text/javascript" src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
+    <script src="{{ STATIC_URL }}/js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
     <script type="text/javascript" src="/min/?f=/js/principal.js,/js/site.js"></script>
 <?php 
     echo $this->createTagsJS($this->links_js_footer);
