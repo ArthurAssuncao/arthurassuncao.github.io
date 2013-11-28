d100ec21505771397111dc16be29338bd5405c42
ArthurAssuncao <arthur.xx92@gmail.com>
2013-11-28 11:34:23 -0200

    adicionado: pagina sobre

---
+++ src/sobre.php
@@ -0,0 +1,16 @@
+<?php
+    require('template/PaginaCached.class.php');
+    $pagina = new PaginaCached(__FILE__);
+    $pagina->setTitle('Sobre - Arthur Assunção');
+    $pagina->setDescription('Pagina de informações sobre o Arthur Assuncao');
+    $pagina->setKeywords('informacoes, sobre, arthur assuncao');
+    $pagina->setCanonical($pagina->createCanonicalLink());
+    $pagina->iniciaConteudo();
+?>
+    <br />
+    Sobre
+
+<?php
+    $pagina->finalizaConteudo();
+    echo $pagina->renderizar();
+?>
